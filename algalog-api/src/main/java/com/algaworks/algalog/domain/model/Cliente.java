f6ede08f2c9dbe5db613efa412ffa33a2fba53dc
claudio-dev1993 <claudinhoroberto2008@gmail.com>
2021-05-22 11:20:56 -0300

    Módulo 2 -> CRUD, Bean Validation, JPA, Handler Excpetion, Domain Services

--- algalog-api/src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,25 +1,51 @@
 package com.algaworks.algalog.domain.model;
 
-import lombok.Getter;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
 @Getter
 @Setter
-
+@Entity
+@Table
 public class Cliente {
-
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
+	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
+	
+	@NotBlank
+	@Size(max = 20)
+	@Column(name = "fone")
 	private String telefone;
-
-	public Cliente(Long id, String nome, String email, String telefone) {
-
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.telefone = telefone;
+	
+	public Cliente () {
+		
 	}
+
+	
 
 }
